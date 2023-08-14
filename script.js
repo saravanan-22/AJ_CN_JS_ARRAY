74c2f085452e099a29a977b04b5b6649bea6d280
saravanan-22 <saravanan.skillsafari@gmail.com>
2023-08-14 00:07:23 -0700

    JavaScript_Class-Notes

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log([].concat(["John", "Jack"], ["Mary", "Robert"], ["Prince"]));
+
+
+
+
+
+
+
+
+
+
+
+
+// 1.[]: This represents an empty array. It's the starting point for the concatenation operation.
+
+// 2.concat(): This is a method available for arrays in JavaScript that is used to concatenate (combine) arrays together. It takes one or more arrays as arguments and returns a new array containing the elements of all the arrays.
+
+// 3. ["John", "Jack"], ["Mary", "Robert"], ["Prince"]: These are three separate arrays, each containing a list of strings. In the context of your example, they are the arrays that you want to concatenate together.
